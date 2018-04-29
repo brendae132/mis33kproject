fdbd4a580863171a45a1986500494b63cd8a6653
Brenda <brendaescobar@utexas.edu>
2018-04-29 17:48:58 -0400

    Added Home/AddMovie view and method in HomeController

--- FinalProject_Team12/FinalProject_Team12/Utilities/GenerateMovieNumber.cs
+++ FinalProject_Team12/FinalProject_Team12/Utilities/GenerateMovieNumber.cs
@@ -7,33 +7,33 @@ using FinalProject_Team12.Models;
 
 namespace FinalProject_Team12.Utilities
 {
-    public static class GenerateMovieID
+    public static class GenerateMovieNumber
     {
-        public static Int32 GetMovieID()
+        public static Int32 GetMovieNumber()
         {
             //db context to connect to database
             AppDbContext db = new AppDbContext();
 
             //current max product sku
-            Int32 intMaxMovieID;
+            Int32 intMaxMovieNumber;
 
             //sku for next product
-            Int32 intNextMovieID;
+            Int32 intNextMovieNumber;
 
             if (db.Movies.Count() == 0) //no products in database yet
             {
-                intMaxMovieID = 3001; //sku starts at 5001
+                intMaxMovieNumber = 3001; //sku starts at 5001
             }
             else
             {
-                intMaxMovieID = db.Movies.Max(p => p.MovieID); //highest number in database right now
+                intMaxMovieNumber = db.Movies.Max(p => p.MovieNumber); //highest number in database right now
             }
 
             //add one to the current max to find the next one
-            intNextMovieID = intMaxMovieID + 1;
+            intNextMovieNumber = intMaxMovieNumber + 1;
 
             //return the value
-            return intNextMovieID;
+            return intNextMovieNumber;
         }
     }
 }
